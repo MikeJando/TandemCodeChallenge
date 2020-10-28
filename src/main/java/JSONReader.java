dd3f3b07dbc0c96b54cde6dacc6aad9a24816015
msjando <msjando@neiu.edu>
2020-10-27 23:05:55 -0500

    Added additional class "TandemQuiz" to make code easier to read.
    Implemented functionality to handle input mismatch and IndexOutOfBounds.

--- src/main/java/JSONReader.java
+++ src/main/java/JSONReader.java
@@ -4,23 +4,19 @@ import org.json.simple.parser.JSONParser;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
-
 
 public class JSONReader
 {
-    public static void main(String[] args)
+    public static HashMap<Integer, HashMap<String,String>> read(Path p)
     {
-        Scanner kbd = new Scanner(System.in);
 
-        Path inPath = Paths.get("src", "main", "resources", "Apprentice_TandemFor400_Data.json").toAbsolutePath();
         JSONParser parser = new JSONParser();
         HashMap<Integer, HashMap<String,String>> map = new HashMap<>();
 
         try
         {
-            Reader reader = Files.newBufferedReader(inPath);
+            Reader reader = Files.newBufferedReader(p);
             Object obj = parser.parse(reader);
             JSONArray jArr = (JSONArray) obj;
 
@@ -34,9 +30,9 @@ public class JSONReader
                 ArrayList<String> list = (JSONArray)x.get("incorrect");
                 String s;
                 if(list.size()==3)
-                    s = list.get(0)+","+list.get(1)+","+list.get(2);
+                    s = list.get(0)+"@"+list.get(1)+"@"+list.get(2);
                 else
-                    s = list.get(0)+","+list.get(1);
+                    s = list.get(0)+"@"+list.get(1);
                 innerMap.put("incorrect",s);
 
                 innerMap.put("correct",(String) x.get("correct"));
@@ -51,50 +47,6 @@ public class JSONReader
             ex.printStackTrace();
         }
 
-        ArrayList<Integer> asked = new ArrayList<>();
-
-        for(int i = 0;i<10;i++)
-        {
-            int num = (int) (Math.random()*map.size());
-            if(!asked.contains(num))
-                asked.add(num);
-            else
-                i--;
-        }
-
-        int score = 0;
-
-        for(int i = 0;i<asked.size();i++)
-        {
-            ArrayList<String> sList = new ArrayList<>();
-            String[] sArr = (map.get(asked.get(i)).get("incorrect")).split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
-            String question = map.get(asked.get(i)).get("question");
-            Collections.addAll(sList,sArr);
-            sList.add(map.get(asked.get(i)).get("correct"));
-
-            Collections.shuffle(sList);
-
-            System.out.println(question);
-            System.out.println();
-            for(int j = 0;j<sList.size();j++)
-            {
-                System.out.println(j+" "+sList.get(j));
-            }
-            int answer = kbd.nextInt();
-
-            if(sList.get(answer)==map.get(asked.get(i)).get("correct"))
-                score++;
-
-            System.out.println();
-            System.out.println("The correct answer was: "+map.get(asked.get(i)).get("correct"));
-            System.out.println();
-
-
-        }
-
-        if(score>=5)
-            System.out.println("Great Job!  You're score was: " + score);
-        else
-            System.out.println("These were some tough ones!  You're score was: " + score);
+        return map;
     }
 }
---
+++ src/main/java/TandemQuiz.java
@@ -0,0 +1,82 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TandemQuiz
+{
+    public static void main(String[] args)
+    {
+        Scanner kbd = new Scanner(System.in);
+        Path inPath = Paths.get("src", "main", "resources", "Apprentice_TandemFor400_Data.json").toAbsolutePath();
+
+        HashMap<Integer, HashMap<String, String>> map = JSONReader.read(inPath);
+
+        ArrayList<Integer> asked = new ArrayList<>();
+
+        for(int i = 0;i<10;i++)
+        {
+            int num = (int) (Math.random()*map.size());
+            if(!asked.contains(num))
+                asked.add(num);
+            else
+                i--;
+        }
+
+        int score = 0;
+        boolean check = false;
+        int answer = 0;
+
+        for(int i = 0;i<asked.size();i++)
+        {
+            ArrayList<String> sList = new ArrayList<>();
+            String[] sArr = (map.get(asked.get(i)).get("incorrect")).split("@");
+            String question = map.get(asked.get(i)).get("question");
+            Collections.addAll(sList,sArr);
+            sList.add(map.get(asked.get(i)).get("correct"));
+
+            Collections.shuffle(sList);
+
+            System.out.println(question);
+            System.out.println();
+            for(int j = 0;j<sList.size();j++)
+            {
+                System.out.println(j+" "+sList.get(j));
+            }
+            System.out.println();
+            System.out.println("What is your guess?");
+            while(!kbd.hasNextInt())
+            {
+                System.out.println("That was not a valid option, please try again: ");
+                kbd.next();
+            }
+            answer = kbd.nextInt();
+            if(answer>=sList.size())
+                check = true;
+
+            while(check)
+            {
+                kbd.nextLine();
+                System.out.println("That was not a valid option, please try again: ");
+                answer = kbd.nextInt();
+                if(answer<sList.size()&& answer>-1)
+                    check = false;
+            }
+
+
+            if(sList.get(answer)==map.get(asked.get(i)).get("correct"))
+                score++;
+
+            System.out.println();
+            System.out.println("The correct answer was: "+map.get(asked.get(i)).get("correct"));
+            System.out.println();
+        }
+
+        if(score>=5)
+            System.out.println("Great Job!  You're score was: " + score);
+        else
+            System.out.println("These were some tough ones!  You're score was: " + score);
+    }
+}
